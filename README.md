2450d5149ee3bc5d4432cdbe9560fccea839d065
Emanuele Paolini <emanuele.paolini@gmail.com>
2022-02-20 19:59:35 +0100

    first version

---
+++ README.md
@@ -0,0 +1,4 @@
+Tesla vortex. See Mathologer video
+https://www.youtube.com/watch?v=6ZrO90AI0c8&ab_channel=Mathologer
+
+See it in action: https://paolini.github.io/vortex/
---
+++ vortex.js
@@ -0,0 +1,58 @@
+const input_d = document.getElementById("input_d");
+const input_m = document.getElementById("input_m");
+
+function draw() {
+    var canvas = document.getElementById("my_canvas");
+    var ctx = canvas.getContext('2d');
+    var d = parseInt(input_d.value);
+    var m = parseInt(input_m.value);
+    var s = parseInt(input_s.value);
+    var center_x = canvas.width / 2;
+    var center_y = canvas.height /2;
+    var r = Math.min(canvas.width, canvas.height)/2-30;
+
+    function x(n, r) {
+        return center_x + r*Math.sin(n*2*Math.PI/d); 
+    }
+
+    function y(n, r) {
+        return center_y - r*Math.cos(n*2*Math.PI/d);
+    }
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    // ctx.lineJoin = "round";
+    ctx.drawStyle = "black";
+    ctx.beginPath();
+    ctx.arc(center_x, center_y, r, 0, 2*Math.PI);
+    ctx.stroke();
+
+    ctx.lineWidth = 2.0;
+    for (var i=0;i<d;++i) {
+        ctx.beginPath();
+        ctx.moveTo(x(i, r), y(i, r));
+        ctx.lineTo(x(m*i, r), y(m*i, r));
+        ctx.stroke()
+    }
+    ctx.fillStyle = "#ff7a00";
+    for (var i=0; i<d; ++i) {
+        ctx.beginPath();
+        ctx.arc(x(i, r), y(i, r), 5, 0, 2*Math.PI);
+        ctx.fill();
+    }
+    if (d<100) {
+        ctx.font = "24px serif";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#ac8455";
+        var R = r+18;
+        for (var i=0; i<d; ++i) {
+            ctx.fillText(''+i, x(i, R), y(i, R));
+        }
+    }
+}
+
+draw();
+input_m.onchange = draw;
+input_d.onchange = draw;
+input_s.onchange = draw;
+
